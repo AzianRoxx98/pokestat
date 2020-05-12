61385ba8ecddded43a93bb37564fc7b9cd751ff4
Daniel <danielcunanan@gmail.com>
2020-05-11 21:00:52 -0400

    One last time, please work you fuck

--- pokebot.py
+++ pokebot.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 bot = commands.Bot(command_prefix='$', description='A bot that greets the user back.')
 
-Connection = SQL.connect(host="192.168.1.148", port = "5432", dbname = "pokemonDB", user="postgres", password="Database1234")
+Connection = SQL.connect(host="ec2-18-213-176-229.compute-1.amazonaws.com", port = "5432", dbname = "d51hgj78fje13v", user="uqmhqntlkzypew", password="38c10d4d2c112be1bfe0d714ba20a2a8c553b92797323e7fbe27fae762e56424")
 Cursor = Connection.cursor()
-print("PostgreSQL Connection Successful")
+print("Heroku PostgreSQL Connection Successful")
 
 @bot.event
 async def on_ready():
